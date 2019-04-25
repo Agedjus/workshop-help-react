bbf4a9c0a7ff25e421b63b7163cd8f4b4da5dad3
Ángel De Jesús García Pineda <adj@kamstrup.com>
2019-04-25 17:53:42 +0200

    Workshop

---
+++ src/ejercicios/ej3/c2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import C3 from "./c3";
+
+class C2 extends React.Component {
+  render() {
+    return (
+      <div className="Display">
+        {this.props.number}
+        <C3 />
+      </div>
+    );
+  }
+}
+
+export default C2;
